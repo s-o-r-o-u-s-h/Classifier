d35f0de3d40f560b50fb5edb06731675549c3328
soroush <soroush.gh2@gmail.com>
2015-02-08 20:26:27 +0330

    Third update, Added some docstrings

--- core/dtree.py
+++ core/dtree.py
@@ -7,18 +7,28 @@ class DTree():
         self.values = values  # list of attribute values
         self.childs = dict.fromkeys(self.values)  # children of a DTree node
 
+    '''     Following function is necessary for adding a subtree to the current tree
+            during the process of creating decision tree.                       '''
+
     def add_child(self, value, child):
         if value in self.values and self.childs[value] is None:
             self.childs[value] = DTree(child.attr_name, child.values)
         else:
             print('not allowed')
 
+    '''     When a tree doesn't have any subtrees,it should have leave(s), Following
+            function helps you to add a leaf to the current tree.               '''
+
     def add_leaf(self, value, leaf):
         if value in self.values and self.childs[value] is None:
             self.childs[value] = leaf
 
+    '''     Sometimes you need a basic printing function to see your tree, This
+            function helps to search a tree without any specific approach we
+            know in Graph search algorithms.                                    '''
+
     def print_tree(self):
-        print('att', self.attr_name, 'vals:')
+        print('att', self.attr_name, 'values:')
         for i in self.values:
             print(i)
             if isinstance(self.childs[i], type(1)):
@@ -27,4 +37,4 @@ class DTree():
                 DTree.print_tree(self.childs[i])
             else:
                 print('None found')
-        print('end of class')
+        print('EOT')
